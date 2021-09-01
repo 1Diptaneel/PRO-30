5ff66a23991ba76039ac7aa2c5f23ff3e189d782
1Diptaneel <83410205+1Diptaneel@users.noreply.github.com>
2021-09-01 18:16:34 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -23,7 +23,7 @@ class Block{
         }
         else{
 
-          //World.remove(world, this.body);
+          World.remove(world, this.body);
           //World.remove(world, this.image);
           // World.add(world, this.body);
           // World.destroy(world, this.body);
